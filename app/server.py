2af42c3a306727ae60df274da6adbb05c53dd740
Dena Metili Mwangi <dena.mwangi@gmail.com>
2019-05-29 14:51:51 -0400

    tweaked for sitcom decades

--- app/server.py
+++ app/server.py
@@ -9,10 +9,13 @@ from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import HTMLResponse, JSONResponse
 from starlette.staticfiles import StaticFiles
 
-export_file_url = 'https://www.dropbox.com/s/6bgq8t6yextloqp/export.pkl?raw=1'
-export_file_name = 'export.pkl'
+# export_file_url = 'https://www.dropbox.com/s/6bgq8t6yextloqp/export.pkl?raw=1'
+# export_file_name = 'export.pkl'
 
-classes = ['black', 'grizzly', 'teddys']
+export_file_url = 'https://drive.google.com/uc?export=download&id=1lVBbtg2hog1IHisFC84uOgTO3QOxqjv8'
+export_file_name = 'test.pkl'
+
+classes = ['50s', '90s']
 path = Path(__file__).parent
 
 app = Starlette()
